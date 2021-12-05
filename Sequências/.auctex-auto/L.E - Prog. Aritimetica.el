43bdefa63a4ff842d63972c9a2b1c37049eb4a7e
fumacci <smoek@tutanota.com>
2021-12-05 11:12:53 -0300

    meu deus como é triste estar vivo

---
+++ Sequências/.auctex-auto/L.E - Prog. Aritimetica.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "L.E - Prog. Aritimetica"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("exam" "12pt" "addpoints")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "brazil")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "exam"
+    "exam12"
+    "babel"))
+ :latex)
+
---
+++ Sequências/.auctex-auto/P.G.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "P.G"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "svgnames") ("inputenc" "utf8") ("babel" "brazil") ("geometry" "tmargin=1.5in" "lmargin=1.25in" "rmargin=1.25in" "bmargin=1.0in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "xcolor"
+    "tcolorbox"
+    "inputenc"
+    "lmodern"
+    "indentfirst"
+    "microtype"
+    "amsmath"
+    "babel"
+    "parskip"
+    "geometry"))
+ :latex)
+
